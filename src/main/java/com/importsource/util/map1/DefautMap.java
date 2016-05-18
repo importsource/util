fb2dc90f97ef6b1cd71b196e54a085bfa8cdcb89
Hezf <Hezf@Hezf-PC>
2016-05-18 16:32:34 +0800

    add array scaleness

--- src/main/java/com/importsource/util/map1/DefautMap.java
+++ src/main/java/com/importsource/util/map1/DefautMap.java
@@ -1,5 +1,6 @@
 package com.importsource.util.map1;
 
+import java.util.Arrays;
 
 /**
  * 默认的map
@@ -9,15 +10,24 @@ package com.importsource.util.map1;
  * @param <V>
  */
 public class DefautMap<K, V> implements Map<K, V> {
-	Entry<?, ?>[] entrySet = new Entry<?, ?>[100];
+	Entry<?, ?>[] entrySet = new Entry<?, ?>[capacity];
+	private static final int capacity=1;
+	private int capacitySum=capacity;
     private int index=0;
     
 	public void put(K key, V value) {
 		Entry<K, V> entry = new Entry<K, V>();
 		entry.setKey(key);
 		entry.setValue(value);
-
-		entrySet[index] = entry;
+		try {
+			entrySet[index] = entry;
+		} catch (IndexOutOfBoundsException e) {
+			capacitySum+=capacity;
+			Entry<?, ?>[] tmp=Arrays.copyOf(entrySet, capacitySum);
+			Entry<?, ?>[] cArr2 = tmp;
+			cArr2[index] = entry;
+			entrySet=cArr2;
+		}
 		index++;
 	}
 
