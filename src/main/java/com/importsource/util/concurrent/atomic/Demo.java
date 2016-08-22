07d1709c7440cec19afe9eba9bdb6e43dc9bc347
importsource <hezhuofan24@126.com>
2016-08-22 11:37:39 +0800

    add lock

---
+++ src/main/java/com/importsource/util/concurrent/atomic/Demo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.importsource.util.concurrent.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Hezf
+ *
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        AtomicInteger
+	}
+
+}
